17e4ba180ac97be458bf69471bc94cbf625eca6d
John Garrett <john.garrett@astro.ox.ac.uk>
2020-01-17 14:29:07 -0500

    Test wavenumber and propagation constant

--- tests/test_tlines.py
+++ tests/test_tlines.py
@@ -1,4 +1,9 @@
-"""Test the transmission line module (``rf.tlines``)."""
+"""Test the transmission line module (``rf.tlines``).
+
+This module includes classes for rectangular and circular waveguides, and
+microstrips.
+
+"""
 
 import numpy as np
 import pytest
@@ -7,24 +12,13 @@ import scipy.constants as sc
 import rftools as rf 
 
 
-def test_cutoff_frequency():
-
-    a = 1 * sc.milli
-    b = a / 2
-    wg = rf.RectangularWaveguide(a, b, verbose=False)
-
-    # print(wg.fc / sc.giga)
-    # print(wg.cutoff('TE10') / sc.giga)
-
-    assert round(wg.fc, 4) == round(wg.cutoff('TE10'), 4)
-
-
 def test_teflon_filled_waveguide():
     """Example 3.1 on page 112 of Pozar."""
 
     a = 1.07 * sc.centi
     b = 0.43 * sc.centi
-    er = 2.08  # teflon
+    er = 2.08  # approx. teflon
+    tand = 0.0004  # loss tangent
     wg = rf.RectangularWaveguide(a, b, er=er, verbose=False)
 
     fc_te10 = wg.cutoff('TE10') / sc.giga
@@ -33,6 +27,7 @@ def test_teflon_filled_waveguide():
     fc_te11 = wg.cutoff('TE11') / sc.giga
     fc_te21 = wg.cutoff('TE21') / sc.giga
 
+    # Debug
     # print(round(fc_te10, 2))
     # print(round(fc_te20, 2))
     # print(round(fc_te01, 2))
@@ -49,18 +44,33 @@ def test_teflon_filled_waveguide():
 def test_teflon_filled_circular_waveguide():
     """Example 3.2 on page 123 of Pozar."""
 
+    # Build waveguide
     a = 0.5 * sc.centi
     er = 2.08  # teflon
     wg = rf.CircularWaveguide(a, er=er, verbose=False)
 
+    # Cutoff frequencies
     fc_te11 = wg.cutoff('TE11') / sc.giga
     fc_tm01 = wg.cutoff('TM01') / sc.giga
 
-    # print(round(fc_te11, 2))
-    # print(round(fc_tm01, 2))
+    # Debug
+    # print("Cutoff TE11: ", round(fc_te11, 2), " GHz")
+    # print("Cutoff TM01: ", round(fc_tm01, 2), " GHz")
 
     assert abs(fc_te11 - 12.19) < 0.025
     assert abs(fc_tm01 - 15.92) < 0.025
+
+    # Properties at 14 GHz
+    freq = 14e9
+    k    = wg.k(freq)
+    beta = wg.beta(freq)
+
+    # Debug
+    # print("Wavenumber:   {:.2f} m-1".format(k))
+    # print("beta:         {:.2f} m-1".format(beta))
+
+    assert abs(k    - 422.9) < 0.3
+    assert abs(beta - 208.0) < 0.6
 
 
 def test_microstrip_parameters():
@@ -113,13 +123,13 @@ def test_microstrip():
 
     ms = rf.Microstrip(er, d, w, verbose=False)
 
+    # Debug
     # print(ms.z0)
 
     assert round(ms.z0, 0) == 50
 
 
 if __name__ == "__main__":
-    test_cutoff_frequency()
     test_teflon_filled_waveguide()
     test_teflon_filled_circular_waveguide()
     test_microstrip_parameters()

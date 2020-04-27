a38da63030e7fd646e74857a566e68876d54991e
John Garrett <john.garrett@astro.ox.ac.uk>
2020-04-27 14:27:32 -0400

    init: add version number

--- rftools/__init__.py
+++ rftools/__init__.py
@@ -2,3 +2,6 @@
 from rftools.network import Network
 import rftools.noise
 from rftools.tlines import RectangularWaveguide, CircularWaveguide, Microstrip
+
+__author__ = "John Garrett"
+__version__ = "0.0.1"

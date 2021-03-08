0550ddac824460efa0c20e6da5f843f739646d39
John Garrett <john.garrett@astro.ox.ac.uk>
2021-03-08 15:55:54 -0500

    init: add skin_depth, mean_free_path, conductivity_4k, conductivity_rough, surface_resistance

--- rftools/__init__.py
+++ rftools/__init__.py
@@ -2,6 +2,7 @@
 from rftools.network import Network
 import rftools.noise
 from rftools.tlines import RectangularWaveguide, CircularWaveguide, Microstrip
+from rftools.misc import skin_depth, mean_free_path, conductivity_4k, conductivity_rough, surface_resistance
 
 __author__ = "John Garrett"
 __version__ = "0.0.3"

60d8730fb7b2d0ada2d28d181ca2bc1bad2b702e
John Garrett <john.garrett@astro.ox.ac.uk>
2021-08-02 10:04:49 -0400

    Add module for uniform plane waves

--- rftools/__init__.py
+++ rftools/__init__.py
@@ -5,6 +5,8 @@ import rftools.noise
 from rftools.tlines import RectangularWaveguide, CircularWaveguide, Microstrip
 from rftools.conduction import (skin_depth, mean_free_path, conductivity_ase,
                                 conductivity_rough, surface_resistance)
+from rftools.upw import (lossless_slab, lossless_slab_at_angle, HDPE_N,
+                         MYLAR_N, TEFLON_N, ZITEX_N)
 
 __author__ = "John Garrett"
 __version__ = "0.0.3"
